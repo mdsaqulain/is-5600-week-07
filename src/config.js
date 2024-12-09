3042d5997b1f412d9a7f77ff045e7a24b6c5943a
Md Saqulain <62801332+mdsaqulain@users.noreply.github.com>
2024-12-09 05:25:22 +0000

    Lab-07-Submission

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // TODO: Replace this with your own Server URL for your codespace
-export const BASE_URL = "https://glowing-space-memory-q7p4jvr9g4p424rj7-3080.app.github.dev";
+export const BASE_URL = "https://psychic-robot-q4rwjw9vr77f6967-3080.app.github.dev";
